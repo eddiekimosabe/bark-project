c720c918345fa73088160f0af86194f0edbcafa3
eddiekimosabe <eddiekimosabe@gmail.com>
2020-02-27 10:43:28 -0500

    Add likes routing and controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,33 @@
+class LikesController < ApplicationController
+	before_action :set_dog, only: [:create]
+
+	def create
+		@like = Like.new(user_id: current_user.id, dog_id: @dog.id)
+		respond_to do |format|
+			if already_liked?
+				format.html { redirect_back fallback_location: root_path, notice: "You can't like more than once"}
+			elsif owner?
+				format.html { redirect_back fallback_location: root_path, notice: "Can't like your own dog"}
+			else
+				if @like.save
+					format.html { redirect_back fallback_location: root_path }
+				else
+					format.html { redirect_back fallback_location: root_path, notice: "Something went wrong"}
+				end
+			end
+		end
+	end
+
+	private
+	def set_dog
+		@dog = Dog.find(params[:dog_id])
+	end
+
+  def already_liked?
+    Like.where(user_id: current_user.id, dog_id: params[:dog_id]).exists?
+  end
+
+  def owner?
+  	current_user == @dog.owner
+  end
+end
