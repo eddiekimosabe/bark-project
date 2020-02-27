76f639e703a7e8b3fe7c7f22de6109d0db47c3e0
eddiekimosabe <eddiekimosabe@gmail.com>
2020-02-27 10:44:58 -0500

    Set up association with user and likes, add per page pagination, eager load for images

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,10 @@
 class Dog < ApplicationRecord
   has_many_attached :images
+  # eager loading to deal with n+1
+  scope :with_eager_loaded_images, -> {eager_load(images_attachments: :blob)}
+  #ensures that when dog gets destroyed all likes associated with dog gets destroyed also
+  has_many :likes, dependent: :destroy
+  belongs_to :owner, class_name: 'User', foreign_key: 'user_id', optional: true
+  # setting how pagination of how many dogs per page
+	self.per_page = 5
 end
