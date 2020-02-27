76c2e29927c95456720988a724d3a75827c921ce
eddiekimosabe <eddiekimosabe@gmail.com>
2020-02-27 10:45:43 -0500

    Add association with dogs and likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_many :dogs
+  #dependent: :destroy ensures that when user gets destroyed all likes associated gets destroyed
+  has_many :likes, dependent: :destroy
 end
